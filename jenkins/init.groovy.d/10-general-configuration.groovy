01c0124c1071bc79b8d62efe9ca4934055a03007
Mikko Piuhola <mikko.piuhola@gmail.com>
2017-05-11 21:14:43 +0300

    Disable CLI remoting, cleanup init scripts

--- jenkins/init.groovy.d/10-general-configuration.groovy
+++ jenkins/init.groovy.d/10-general-configuration.groovy
@@ -4,30 +4,39 @@
 // You can add your own by copying them into
 // /usr/share/jenkins/ref/init.groovy.d/
 
-import hudson.model.*;
 import jenkins.model.*;
+import jenkins.CLI;
 
 def instance = Jenkins.getInstance()
 def env = System.getenv()
 def jlc = JenkinsLocationConfiguration.get()
 
-// Configure Jenkin's external URL
-if (jlc.getUrl() != env['CONF_JENKINS_URL']) {
-  println "--> setting Jenkins url to ${env['CONF_JENKINS_URL']}"
-  jlc.setUrl(env['CONF_JENKINS_URL'])
-  jlc.save()
-}
-
-// Set number of executors on master
+def url = env['CONF_JENKINS_URL']
 def execCount = env['CONF_JENKINS_NUM_EXECUTORS']?.toInteger() ?: 4
-println "--> setting number of executors on master to ${execCount}"
-instance.setNumExecutors(execCount)
+def slaveAgentPort = env['JENKINS_SLAVE_AGENT_PORT']?.toInteger() ?: 49187
 
 Thread.start {
+
+  // Configure Jenkin's external URL
+  if (jlc.getUrl() != url) {
+    println "--> setting Jenkins url to ${url}"
+    jlc.setUrl(url)
+    jlc.save()
+  }
+
+  // Jenkins 2.54+ disables CLI remoting due to security and performance issues
+  // See: https://jenkins.io/blog/2017/04/11/new-cli/
+  // but at least currently it is only done in the SetupWizard which we are skipping.
+  println '--> disabling CLI remoting for security'
+  CLI.get().setEnabled(false);
+
+  println "--> setting number of executors on master to ${execCount}"
+  instance.setNumExecutors(execCount)
+
   sleep 10000 // Wait for Jenkins instance to boot
-  println "--> setting agent port for jnlp"
-  int port = env['JENKINS_SLAVE_AGENT_PORT'].toInteger()
-  instance.setSlaveAgentPort(port)
+  println "--> setting agent port for jnlp to ${slaveAgentPort}"
+  instance.setSlaveAgentPort(slaveAgentPort)
   instance.save()
   println "--> setting agent port for jnlp... done"
+
 }
